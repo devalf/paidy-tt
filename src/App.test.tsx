858e0c3eb7faf502a162692037ed8611230d33d3
Oleksiy Petrychko <mykolasich@gmail.com>
2023-06-06 21:31:17 +0300

    Project initial setup and configurations

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,10 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('Should render `Home` page with a single button', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+
+  const linkElement = screen.getByText(/Open Modal/i);
+
   expect(linkElement).toBeInTheDocument();
 });
